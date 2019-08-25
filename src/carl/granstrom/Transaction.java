d5e26487c080b9c2e28f0d3e69c5268da4e7ad6c
Carl Granstrom <carl.grnstrm@gmail.com>
2019-08-25 11:31:30 +0200

    Bit more work, stopped in the middle of the transaction constructor.

--- src/carl/granstrom/Transaction.java
+++ src/carl/granstrom/Transaction.java
@@ -1,4 +1,32 @@
 package carl.granstrom;
 
+import java.util.Random;
+
 public class Transaction {
+    private Item[] transaction;
+    private Long transactionID;
+    private static Long idCounter = 0l;
+    private static Random random = new Random();
+
+    Transaction(){
+        idCounter++;
+    }
+
+    Transaction(boolean randomize){
+        idCounter++;
+
+        this.transactionID = idCounter;
+
+        int width = random.nextInt(6) + random.nextInt(5) + 1;  //max transaction width of 10
+        Long[] transaction = new Long[width];
+
+        //KEEP WORKING HERE
+        int counter = 0;
+        for (Long itemId : transaction){
+            int itemNr = random.nextInt(20) + 1;
+
+        }
+
+
+    }
 }
